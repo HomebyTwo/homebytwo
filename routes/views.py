4080d637978a2a962fa55a26a6778ecbef42146b
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-06-22 19:41:10 +0200

    Still playing around

--- routes/views.py
+++ routes/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.gis.measure import D, Distance
+from django.contrib.gis.db.models.functions import Length
 
 from .models import Route
 
 # Create your views here.
 def index(request):
-    routes = Route.objects.order_by('-created')
+    routes = Route.objects.annotate(distance=Length('geom')).order_by('-created')
     context = {
         'routes': routes,
     }
     return render(request, 'routes/index.html', context)
 
 def detail(request, route_id):
-    return HttpResponse("You're looking at route %s." % route_id)
+    route = Route.objects.get(id=route_id)
+    context = {
+        'route': route,
+    }
+    return render(request, 'routes/detail.html', context)
 
 def edit(request, route_id):
     response = "You are looking at the edit page of route %s"
