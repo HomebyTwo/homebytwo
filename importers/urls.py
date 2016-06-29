8a265c04c553bfb7758d0644c6ad4e05fbb2a0c6
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-06-29 10:11:01 +0200

    Create SwitzerlandMobilityRoute Class

--- importers/urls.py
+++ importers/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^strava/$', views.strava_index, name='strava_index'),
     url(r'^strava/authorized/$', views.strava_authorized, name='strava_authorized'),
     url(r'^strava/routes/(?P<strava_route_id>[0-9]+)/$', views.strava_detail, name='strava_edit'),
+    url(r'^strava/$', views.switzerland_mobility_index, name='switzerland_mobility_index'),
 ]
