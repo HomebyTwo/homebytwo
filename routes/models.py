2f035e3728780123349b852dadaadbe6ee987cad
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-06-29 10:07:33 +0200

    Get place elevation from gmaps

--- routes/models.py
+++ routes/models.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.contrib.gis.db import models
 from django.contrib.gis.measure import Distance
 from django.contrib.staticfiles import finders
 from autoslug import AutoSlugField
+
+import googlemaps
 
 
 class Route(models.Model):
@@ -42,11 +45,30 @@ class Place(models.Model):
     type = models.CharField(max_length=50)
     altitude = models.FloatField()
     name = models.CharField(max_length=250)
-    geom = models.MultiPointField(srid=21781)
     description = models.TextField('Text description of the Place', default='')
     updated = models.DateTimeField('Time of last update', auto_now=True)
     created = models.DateTimeField('Time of last creation', auto_now_add=True)
     lang = models.CharField(max_length=50)
+
+    geom = models.PointField(srid=21781)
+
+    #returns altitude for a place and updates the database entry
+    def get_gmaps_elevation(self):
+        #extract first geometry from Multipoint
+        geom = self.geom
+
+        #transform coords to Gmaps SRID
+        geom.transform(4326)
+
+        #Query gmaps API for altitude
+        gmaps = googlemaps.Client(key=settings.GOOGLEMAPS_API_KEY)
+        result = gmaps.elevation(geom.coords)
+
+        #Update altitude information for point
+        self.altitude = result[0]['elevation']
+        self.save()
+
+        return self.altitude
 
 class Segment(models.Model):
     start_place = models.ForeignKey(Place, on_delete=models.PROTECT, related_name='starts')
