f2cc8998986f7a4e949858600f3c984cde2fa189
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-01 21:36:36 +0200

    Update Strava importers views

--- importers/views.py
+++ importers/views.py
@@ -2,14 +2,19 @@ from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from stravalib.client import Client
-from polyline import decode
 import json
 
+from .models import StravaRoute
+
 def index(request):
+
+    # Initialize stravalib client
     client = Client()
+
+    # Generate Strava authorization URL
     strava_authorize_url = client.authorization_url(
         client_id=settings.STRAVA_CLIENT_ID,
-        redirect_uri='http://127.0.0.1:8000/importers/strava/authorized',
+        redirect_uri='/importers/strava/authorized',
     )
 
     context = {
@@ -18,22 +23,41 @@ def index(request):
     return render(request, 'importers/index.html', context)
 
 def strava_authorized(request):
+    # Initialize stravalib client
     client = Client()
+
+    #Obtain access token
     code = request.GET.get('code', '')
-    access_token = client.exchange_code_for_token(client_id=settings.STRAVA_CLIENT_ID, client_secret=settings.STRAVA_CLIENT_SECRET, code=code)
+    access_token = client.exchange_code_for_token(
+                        client_id=settings.STRAVA_CLIENT_ID,
+                        client_secret=settings.STRAVA_CLIENT_SECRET,
+                        code=code,
+                    )
 
     client.access_token = access_token
-    athlete = client.get_athlete()
-
-    response = "<p>For {id}, I now have an access token {token}</p>".format(id=athlete.id, token=access_token)
 
     return HttpResponseRedirect('/importers/strava')
 
 def strava_index(request):
-    client = Client()
-    client.access_token = settings.STRAVA_ACCESS_TOKEN
+    # Check if we have a Strava access token
+    access_token = settings.STRAVA_ACCESS_TOKEN
+
+    if not access_token:
+        #render the Strava connect button
+        return render(request, 'importers/strava/connect.html')
+
+    else:
+        client = Client()
+        client.access_token = access_token
+
     athlete = client.get_athlete()
-    routes = client.get_routes()
+
+    # Retrieve routes from DB
+    routes = StravaRoute.objects.all()
+
+    if not routes:
+        routes = StravaRoute.objects.get_routes_list_from_server()
+
 
     context = {
         'athlete': athlete,
