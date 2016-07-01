55983e5b0fd39f620d72790332c79049eb932420
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-01 22:21:19 +0200

    Get Strava route details

--- importers/models/stravaroute.py
+++ importers/models/stravaroute.py
@@ -39,11 +39,11 @@ class StravaRouteManager(models.Manager):
                 strava_route_id=route.id,
                 defaults={
                         'name': route.name,
-                        'totalup': route.elevation_gain.num,
+                        'totalup': unithelper.meters(route.elevation_gain),
                         'totaldown': 0,
                         'length': unithelper.meters(route.distance),
                         'geom': line,
-                        'description': '',
+                        'description': route.description,
                         'type': route.type,
                         'sub_type': route.sub_type,
                         'strava_timestamp': route.timestamp,
@@ -76,5 +76,31 @@ class StravaRoute(Route):
         # Get Strava access token
         client.access_token = settings.STRAVA_ACCESS_TOKEN
 
+        # Retrieve route detail
         route = client.get_route(self.strava_route_id)
-        route.geom = LineString(decode(route.map.summary_polyline))
+
+        # Decode polyline
+        coords = decode(route.map.polyline)
+
+        # Inverse tupple because Google Maps works in lat, lng
+        coords = [(lng, lat) for lat, lng in coords]
+
+        # Create line string and specify SRID
+        line = LineString(coords)
+        line.srid = 4326
+
+        if len(self.geom) != len(line):
+            self.geom = line
+
+        if route.timestamp != self.strava_timestamp:
+            self.name = route.name
+            self.totalup = unithelper.meters(route.elevation_gain)
+            self.totaldown = 0
+            self.length = unithelper.meters(route.distance)
+            self.geom = line
+            self.description = route.description
+            self.type = route.type
+            self.sub_type = route.sub_type
+            self.strava_timestamp = route.timestamp
+
+        self.save()
