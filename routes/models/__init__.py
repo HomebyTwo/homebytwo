c8c71cc575ad26851d4c98b664d6e9118498c8be
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-02 21:09:18 +0200

    Models for athlete and activity

--- routes/models/__init__.py
+++ routes/models/__init__.py
@@ -1,4 +1,5 @@
 from .route import Route  # NOQA
 from .place import Place  # NOQA
 from .segment import Segment  # NOQA
-from .athlete import Athlete  # NOQA
+from .activity import ActivityType  # NOQA
+from .athlete import Athlete, ActivityPerformance  # NOQA
