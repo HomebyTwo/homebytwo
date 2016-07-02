60d0fef2f67b318cdb9335f5839952deccd7adc3
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-02 21:59:02 +0200

    Remove description and add default values

--- routes/models/activity.py
+++ routes/models/activity.py
@@ -3,4 +3,17 @@ from django.contrib.gis.db import models
 
 class ActivityType(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+
+    # Default values for ActivityPerformance
+    # Vertical speed up used for time calculation in meters/hour
+    default_vam_up = models.FloatField(default=400)
+    # Vertical speed down used for time calculation in meters/hour
+    default_vam_down = models.FloatField(default=2000)
+    # Horizontal speed used for time calculation in meters/hour
+    default_flat_pace = models.FloatField(default=4000)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
