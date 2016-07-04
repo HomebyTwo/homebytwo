cb8403ab8f526a7473693c7459d98978a09902a5
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-04 07:47:54 +0200

    Fix Strava flow

--- routes/models/activity.py
+++ routes/models/activity.py
@@ -17,3 +17,16 @@ class ActivityType(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class ActivityPerformance(models.Model):
+    # Intermediate model for athlete - activity type
+    athlete = models.ForeignKey('Athlete', on_delete=models.CASCADE)
+    activity_type = models.ForeignKey('ActivityType', on_delete=models.CASCADE)
+
+    # Vertical speed up used for time calculation in meters/hour
+    vam_up = models.FloatField(default=400)
+    # Vertical speed down used for time calculation in meters/hour
+    vam_down = models.FloatField(default=2000)
+    # Horizontal speed used for time calculation in meters/hour
+    flat_pace = models.FloatField(default=4000)
