3794124d3f410d1cd864de71874fcc8191ab23b0
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-18 20:10:07 +0200

    Test WIP

--- importers/tests.py
+++ importers/tests.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
+from importers.models import StravaRoute
+
+from django.contrib.gis.geos import LineString
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class StravaRouteTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='tester', email='test@homebytwo.ch', password='top_test')
+
+        StravaRoute.objects.create(
+            strava_route_id=1234567,
+            name='Test Route',
+            totalup='12345',
+            totaldown='0',
+            length='12345',
+            geom=LineString((0, 0), (0, 12345)),
+            description='Test Description',
+            type=1,
+            sub_type=1,
+            user=self.user,
+            timestamp='',
+        )
+
+    def test_strava_route_timestamp_is_set_to_now(self):
+        """timestamp is set to now by default"""
+        route = StravaRoute.objects.get(strava_route_id=1234567)
+        self.assertEqual(route.timestamp, '')
