c382a7fe40f6ded8fb10683666698bf52dbd1db9
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-07-18 23:06:03 +0200

    First request to Swiss Public Transport API

--- routes/tests.py
+++ routes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Place
+
+from datetime import datetime
 
 # Create your tests here.
+
+
+class PlaceTestCase(TestCase):
+    def test_non_public_transport_place_raise_exception(self):
+        no_public_transport = Place.objects.filter(public_transport=False).first()
+        time = datetime.now()
+        no_public_transport.get_timetable_info(time, 'Lausanne')
