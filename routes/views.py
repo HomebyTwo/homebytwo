35ec45d0bdffac5c123d9afb674f71d78a7e9655
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-11-30 16:45:37 +0100

    Improve display of route list

--- routes/views.py
+++ routes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.contrib.gis.measure import D, Distance
 
 from .models import Route
 
+
 def index(request):
-    routes = Route.objects.order_by('-created')
+    routes = Route.objects.order_by('name')
     context = {
         'routes': routes,
     }
     return render(request, 'routes/index.html', context)
+
 
 def detail(request, slug):
     route = get_object_or_404(Route, slug=slug)
@@ -18,12 +19,14 @@ def detail(request, slug):
     }
     return render(request, 'routes/detail.html', context)
 
+
 def by_id(request, route_id):
     route = Route.objects.get(id=route_id)
     context = {
         'route': route,
     }
     return render(request, 'routes/detail.html', context)
+
 
 def edit(request, route_id):
     response = "You are looking at the edit page of route %s"
