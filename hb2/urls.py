16f088128de2b2b88a52c281889bf9f36c394058
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-11-30 14:03:52 +0100

    Remove unused import

--- hb2/urls.py
+++ hb2/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from routes.models import Route
 
 urlpatterns = [
     url(r'^routes/', include('routes.urls')),
