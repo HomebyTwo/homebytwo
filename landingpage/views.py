ec7c75adb32fce3bded5c2ef1223dd7353028cb8
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-12-01 09:08:58 +0100

    HB2-13 email signup WIP

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+from .forms import EmailForm
+
 
 def home(request):
     # Project landing page
     return render(request, 'landingpage/home.html')
+
+
+def get_email(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EmailForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
