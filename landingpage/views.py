b4699edc252c717c0db9e0034294397bf95cae3d
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-12-11 08:48:14 +0100

    HB2-35 Force AJAX submission

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 from .forms import EmailSubscriptionForm
+import json
 
 
 def home(request):
@@ -14,16 +15,14 @@ def home(request):
 
 def email_signup(request):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_ajax():
         # create a form instance and populate it with data from the request:
         form = EmailSubscriptionForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             response = form.signup_email()
-            return HttpResponse(response)
+            return HttpResponse(json.dumps(response))
 
-    # if a GET (or any other method) we'll create a blank form
+    # if a GET (or any other method) we'll redirect to the homepage
     else:
-        form = EmailSubscriptionForm()
-
-    return render(request, 'home.html', {'form': form})
+        return HttpResponseRedirect('/')
