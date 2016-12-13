ed733754d671df65ac904f5652d0dd087e8ce2e7
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-12-13 13:34:03 +0100

    HB2-35 Add tests 🔥

--- landingpage/tests.py
+++ landingpage/tests.py
@@ -1,10 +1,20 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from .forms import EmailSubscriptionForm
+from django.conf import settings
+
+import httpretty
 
 
 class LandingpageTest(TestCase):
-    # models test
+
+    # Construct MailChimp Base URI from the API key
+    api_key = settings.MAILCHIMP_API_KEY
+    datacenter = api_key[api_key.find('-')+1:]
+    api_base_url = 'https://%s.api.mailchimp.com/3.0' % datacenter
+
+    # Get MailChimp List ID from Settings
+    list_id = settings.MAILCHIMP_LIST_ID
 
     # views
     def test_landingpage_home_view(self):
@@ -23,12 +33,47 @@ class LandingpageTest(TestCase):
 
         self.assertRedirects(resp, "/")
 
-    def test_post_email_signup_view(self):
-        content = ''
+    def test_post_email_signup_view_invalid(self):
+        content = 'An error has occured'
         url = reverse("email-signup")
         resp = self.client.post(url)
         self.assertEqual(resp.status_code, 200)
-        self.assertTrue(content in resp.content)
+        self.assertTrue(content in str(resp.content))
+
+    @httpretty.activate
+    def test_post_email_signup_view_not_subscribed(self):
+
+        post_url = '/'.join([self.api_base_url, 'lists',
+                            self.list_id, 'members/'])
+
+        # Intercept request to MailChimp with httpretty
+        httpretty.register_uri(httpretty.POST, post_url, status=200)
+
+        data = {'email': 'example@example.com', 'list_id': self.list_id}
+        content = 'subscribed'
+        url = reverse("email-signup")
+        resp = self.client.post(url, data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(content in str(resp.content))
+
+    @httpretty.activate
+    def test_post_email_signup_view_already_subscribed(self):
+
+        data = {'email': 'example@example.com', 'list_id': self.list_id}
+        post_url = '/'.join([self.api_base_url, 'lists',
+                            self.list_id, 'members/'])
+        search_url = self.api_base_url + '/search-members?query=%s' % data['email']
+
+        # Intercept request to MailChimp with httpretty
+        httpretty.register_uri(httpretty.POST, post_url, status=400)
+        json = '{"exact_matches":{"members":[{"status":"subscribed"}]}}'
+        httpretty.register_uri(httpretty.GET, search_url, body=json)
+
+        content = 'again!'
+        url = reverse("email-signup")
+        resp = self.client.post(url, data)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(content in str(resp.content))
 
     # forms
     def test_valid_form(self):
