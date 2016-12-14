47f47ffbf8e6f6c4a640451632f5ad5a6de712c8
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-12-14 14:53:47 +0100

    HB2-28 Get the env vars out of the tests

--- landingpage/tests.py
+++ landingpage/tests.py
@@ -3,20 +3,28 @@ from django.core.urlresolvers import reverse
 from .forms import EmailSubscriptionForm
 from django.conf import settings
 
+from django.core.exceptions import ImproperlyConfigured
+
+
 import httpretty
 
 
 class LandingpageTest(TestCase):
 
     # Construct MailChimp Base URI from the API key
-    api_key = settings.MAILCHIMP_API_KEY
+    api_key = 'dummy-usXX'
     datacenter = api_key[api_key.find('-')+1:]
     api_base_url = 'https://%s.api.mailchimp.com/3.0' % datacenter
+    list_id = '123456'
 
-    # Get MailChimp List ID from Settings
-    list_id = settings.MAILCHIMP_LIST_ID
+    def setUp(self):
+        settings.MAILCHIMP_API_KEY = self.api_key
+        settings.MAILCHIMP_LIST_ID = self.list_id
 
-    # views
+    # Set MAILCHIMP List ID
+    settings.MAILCHIMP_LIST_ID = '123456'
+
+    # Home view
     def test_landingpage_home_view(self):
         content = "Home by Two"
         signup_form = EmailSubscriptionForm()
@@ -44,7 +52,7 @@ class LandingpageTest(TestCase):
 
         self.assertFalse(gtm_url in str(resp.content))
 
-
+    # Email signup
     def test_get_email_signup_view(self):
         url = reverse("email-signup")
         resp = self.client.get(url)
@@ -57,6 +65,19 @@ class LandingpageTest(TestCase):
         resp = self.client.post(url)
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(content in str(resp.content))
+
+    # Mailchimp API
+    def test_exception_if_mailchimp_list_id_not_set(self):
+        settings.MAILCHIMP_LIST_ID = ''
+
+        url = reverse("home")
+        self.assertRaises(ImproperlyConfigured, self.client.get, url)
+
+    def test_exception_if_mailchimp_api_key_not_set(self):
+        settings.MAILCHIMP_API_KEY = ''
+
+        url = reverse("email-signup")
+        self.assertRaises(ImproperlyConfigured, self.client.post, url)
 
     @httpretty.activate
     def test_post_email_signup_view_not_subscribed(self):
