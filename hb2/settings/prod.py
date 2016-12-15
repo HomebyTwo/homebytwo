2268451f4c5097f512faf116e1b588a9575c1eeb
Cedric Hofstetter <cedric.hofstetter@liip.ch>
2016-12-15 14:46:43 +0100

    Force MailChimp API key in Production

--- hb2/settings/prod.py
+++ hb2/settings/prod.py
@@ -1,1 +1,10 @@
 from .base import *  # NOQA
+from . import get_env_variable
+
+
+###########################
+# Force Mailchimp API Key #
+###########################
+
+MAILCHIMP_API_KEY = get_env_variable('MAILCHIMP_API_KEY')
+MAILCHIMP_LIST_ID = get_env_variable('MAILCHIMP_LIST_ID')
