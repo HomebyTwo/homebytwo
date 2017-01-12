5b970becf4058409d816ca2c58a9ab64cf246a22
Cedric Hofstetter <cedric.hofstetter@mac.com>
2017-01-12 15:16:27 +0100

    HB2-43 require env vars in production

--- hb2/settings/prod.py
+++ hb2/settings/prod.py
@@ -8,3 +8,10 @@ from . import get_env_variable
 
 MAILCHIMP_API_KEY = get_env_variable('MAILCHIMP_API_KEY')
 MAILCHIMP_LIST_ID = get_env_variable('MAILCHIMP_LIST_ID')
+
+#######################################
+# Force Switzerland Mobility Settings #
+#######################################
+
+SWITZERLAND_MOBILITY_LIST_URL = get_env_variable('SWITZERLAND_MOBILITY_LIST_URL')
+SWITZERLAND_MOBILITY_LOGIN_URL = get_env_variable('SWITZERLAND_MOBILITY_LOGIN_URL')
