2c9dacfc068a3cc0a92a37af001a4882332c4e42
Cedric Hofstetter <cedric.hofstetter@mac.com>
2017-03-21 22:19:39 +0100

    fabfile fix for wsgi restart

--- fabfile.py
+++ fabfile.py
@@ -112,8 +112,7 @@ def restart_process():
     """
     Restart the WSGI process by touching the wsgi.py file.
     """
-    run('touch %s' % os.path.join(get_project_root(), env.project_name,
-                                  'wsgi.py'))
+    run('touch %s' % os.path.join(get_project_root(), 'hb2', 'wsgi.py'))
 
 
 def generate_secret_key():
