6990592f1a1a0dbf8de0ce95a8b113e2ab59bad7
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2020-08-23 00:53:49 +0200

    Import Strava activities & streams when logging in

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,17 +2,15 @@ from . import get_env_variable
 from .base import *  # NOQA
 
 
-DEBUG = bool(get_env_variable('DEBUG', True))
+DEBUG = bool(get_env_variable("DEBUG", True))
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
-SECRET_KEY = 'notverymuchsecret'
+SECRET_KEY = "notverymuchsecret"
 
 INSTALLED_APPS += (
-    'debug_toolbar',
-    'django_extensions',
+    "debug_toolbar",
+    "django_extensions",
 )
 
-MIDDLEWARE += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
-INTERNAL_IPS = ('127.0.0.1', '10.10.10.10')
+INTERNAL_IPS = ("127.0.0.1", "10.10.10.10")
