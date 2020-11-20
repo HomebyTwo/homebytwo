055b13afcfc5995c9d5b3fbf60a3db10f1305d5b
Jonathan Macheret <jonathan.macheret@gmail.com>
2020-11-20 23:44:30 +0100

    Allow to have mapbox when outside of Swiss

--- assets/javascripts/app/LeafletMap.js
+++ assets/javascripts/app/LeafletMap.js
@@ -7,21 +7,40 @@ export default class LeafletMap {
     return 4;
   }
 
+  static getSwissMapBoundingBox(leaflet) {
+    return leaflet.latLngBounds(leaflet.latLng(45.398181, 5.140242), leaflet.latLng(48.230651, 11.47757));
+  }
+
   constructor(leaflet, config, disabledServices = []) {
-    const map = leaflet.map(config.id, {crs: leaflet.CRS.EPSG21781});
+    const map = leaflet.map(config.id);
 
     const route = leaflet.geoJson(config.routeGeoJson, {
       style: {color: LeafletMap.ROUTE_COLOR, weight: LeafletMap.ROUTE_WEIGHT}
     });
+
+    if (LeafletMap.getSwissMapBoundingBox(leaflet).contains(route.getBounds())) {
+      map.options.crs = leaflet.CRS.EPSG2056;
+      const greyTopoLayer = leaflet.tileLayer.swiss({layer: 'ch.swisstopo.pixelkarte-grau'}).addTo(map);
+      const colorTopoLayer = leaflet.tileLayer.swiss();
+      const baseMaps = {
+        'Grey': greyTopoLayer,
+        'Color': colorTopoLayer,
+      };
+      leaflet.control.layers(baseMaps, {}).addTo(map);
+    } else {
+      leaflet.tileLayer(config.mapBoxTileUrl, {
+        attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        tileSize: 512,
+        zoomOffset: -1,
+      }).addTo(map);
+    }
+
     route.addTo(map);
     map.fitBounds(route.getBounds());
 
     disabledServices.forEach(service => map[service].disable());
 
-    // Add Swiss layer with default options
-    leaflet.tileLayer.swiss({layer: 'ch.swisstopo.pixelkarte-grau', crs: leaflet.CRS.EPSG21781}).addTo(map);
-
-    const icon = L.divIcon({className: config.divIconClassName});
+    const icon = leaflet.divIcon({className: config.divIconClassName});
     document.querySelectorAll(config.markersSelector).forEach(place => {
       leaflet.geoJson(JSON.parse(place.dataset.geom), {
         pointToLayer: (feature, latlng) => leaflet.marker(latlng, {
